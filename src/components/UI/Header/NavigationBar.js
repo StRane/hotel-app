8ac71c71c8b3e32ef69d0c1db2d1fb05a32217bf
StRane <73998688+StRane@users.noreply.github.com>
2022-02-10 03:16:46 +0700

    Header routing
    NavLink component to match url for active-inactive cases of styling with CSS modules

--- src/components/UI/Header/NavigationBar.js
+++ src/components/UI/Header/NavigationBar.js
@@ -1,13 +1,42 @@
-import NavIcons from "./NavIcons";
+import { NavLink } from "react-router-dom";
+
+import Icon from "../../utilities/Icon";
 import styles from "./NavigationBar.module.css";
 import logo from "./NavBar__icon.png";
 
+const NavElements = ["icon-bookmark", "icon-chat", "icon-home"];
+
 const NavigationBar = () => {
   return (
-    <div className={styles.bar}>
+    <header className={styles.header}>
       <img src={logo} alt={`Hotel App`} className={styles.logo} />
-      <NavIcons />
-    </div>
+      <nav>
+        <ul>
+          <li>
+            <NavLink
+              to="bookmark"
+              className={({ isActive }) => (isActive ? `${styles.active}` : "")}
+            >
+              <Icon name={NavElements[0]} />
+              Bookmarks
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="help"
+              className={({ isActive }) => (isActive ? `${styles.active}` : "")}
+            >
+              <Icon name={NavElements[1]} />
+              Help
+            </NavLink>
+          </li>
+          <li>
+            <Icon name={NavElements[2]} />
+            Login or singup
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
